f05b5836f7ff650f8be7fa8db66ef64c65bdac82
The Manager <The Manager@Dell>
2022-11-15 19:18:49 +0530

    commiting 3rd time

--- src/welcome/FirstP.java
+++ src/welcome/FirstP.java
@@ -4,6 +4,7 @@ public class FirstP {
 
 	public static void main(String[] args) {
 		System.out.println("pull the code");
+		System.out.println("push the code");
 		System.out.println("hi");
 		System.out.println("hi");
 
