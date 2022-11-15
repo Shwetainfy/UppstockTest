76360c44d6c884c08a9700da1c7a5d808609d380
Shwetainfy <118246861+Shwetainfy@users.noreply.github.com>
2022-11-15 19:15:29 +0530

    Update FirstP.java

--- src/welcome/FirstP.java
+++ src/welcome/FirstP.java
@@ -3,6 +3,7 @@ package welcome;
 public class FirstP {
 
 	public static void main(String[] args) {
+		System.out.println("pull the code");
 		System.out.println("hi");
 		System.out.println("hi");
 
